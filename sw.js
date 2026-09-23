9ef2531fccdcf5ae943da8d5b9394181cca3d7f0
Bigthink_Yua <thinkyua@gmail.com>
2026-09-23 14:57:11 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+// G2/G3 일정 – 오프라인 캐시 (인터넷 없이도 화면이 뜨게)
+const CACHE = 'g2g3-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  // 구글 스크립트·시트 요청은 항상 네트워크
+  if (url.hostname.indexOf('google') >= 0) return;
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const copy = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+      return r;
+    }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+  );
+});
